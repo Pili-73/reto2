00770705aa2b6f76bd8a5741f493c2e1fb27ae72
pilar <piruletaabad2002@gmail.com>
2024-10-31 09:18:01 +0100

    main hecho

--- Reto2/src/retos/Reto2.java
+++ Reto2/src/retos/Reto2.java
@@ -7,27 +7,26 @@ public class Reto2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int conversion = -1;
-		boolean mal=false;
 		do {
-			mal = false;
-			System.out.println("Selecciona el tipo de conversion:");
-			System.out.println("1. Longitud");
-			System.out.println("2. Peso");
-			System.out.println("3. Temperatura");
-			System.out.println("4. Volumen");
-			System.out.println("0. Salir");
-
-			try {
-				String s = sc.nextLine();
-				conversion = Integer.parseInt(s);
-				if (conversion < 0 || conversion > 4) {
-					mal = true;
-					System.out.println("Entre 0 y 4");
-				}
-			} catch (Exception e) {
-				System.out.println("Formato inválido");
-				mal = true;
+			conversion = Funciones.dimeEntero(
+					"Selecciona el tipo de conversion: \n1. Longitud \n2. Peso \n3. Temperatura \n4. Volumen \n0. Salir", sc);
+			
+			if (conversion == 1) {
+				Funciones1.Longitud();
 			}
+			
+			if (conversion == 2) {
+				Funciones1.Peso();
+			}
+			
+			if (conversion == 3) {
+				Funciones2.Temperatura();
+			}
+			
+			if (conversion == 4) {
+				Funciones2.Volumen();
+			}
+			
 		} while (conversion != 0);
 		
 		sc.close();
