e4c4a8380d517765485873d223778b16a8a34e0a
mariopellicer <marioopelli23@gmail.com>
2024-10-31 09:25:33 +0100

    funciones 1

--- Reto2/src/retos/Funciones1.java
+++ Reto2/src/retos/Funciones1.java
@@ -22,24 +22,26 @@ public class Funciones1 {
 					sc);
 			if (opcion == 1) {
 				millas = Funciones.dimeDouble("introduce las  millas que quieres convertir", sc);
-				kilometros=Funciones1.millas_Kilometros(millas);
+				kilometros = Funciones1.millas_Kilometros(millas);
 				System.out.println(millas + "millas  son : " + kilometros + "Km");
 			} else if (opcion == 2) {
 				kilometros = Funciones.dimeDouble("introduce los kilometros que quieres convertir", sc);
-				millas=Funciones1.kilometros_Millas(kilometros);
+				millas = Funciones1.kilometros_Millas(kilometros);
 				System.out.println(millas + "millas  son : " + kilometros + "Km");
 			} else if (opcion == 0) {
 				System.out.println("has salido fuera");
 			}
 		} while (opcion != 0);
 	}
-public static double Kilogramos_libras(double libras ) {
-	return libras *=2.2046;
-}
-public static double libras_Kilogramos(double kilogramos) {
-	return kilogramos/=2.2046;
-}
-	
+
+	public static double Kilogramos_libras(double libras) {
+		return libras *= 2.2046;
+	}
+
+	public static double libras_Kilogramos(double kilogramos) {
+		return kilogramos /= 2.2046;
+	}
+
 	public static void Peso() {
 		Scanner sc = new Scanner(System.in);
 		double kilogramos, libras;
@@ -50,15 +52,16 @@ public static double libras_Kilogramos(double kilogramos) {
 					sc);
 			if (opcion == 1) {
 				kilogramos = Funciones.dimeDouble("introduce los kilogramos que quieres convertir", sc);
-				libras=Funciones1.millas_Kilometros(kilogramos);
+				libras = Funciones1.millas_Kilometros(kilogramos);
 				System.out.println(kilogramos + "kg  son : " + libras + "lb");
 			} else if (opcion == 2) {
 				libras = Funciones.dimeDouble("introduce los kilometros que quieres convertir", sc);
-				kilogramos=Funciones1.kilometros_Millas(libras);
+				kilogramos = Funciones1.kilometros_Millas(libras);
 				System.out.println(libras + "lb  son : " + kilogramos + "Kg");
 			} else if (opcion == 0) {
 				System.out.println("has salido fuera");
 			}
+
 		} while (opcion != 0);
 	}
 }
