010aad08da95d65362c3558688db5384def840a7
pilar <piruletaabad2002@gmail.com>
2024-10-31 08:50:48 +0100

    Añadida funcion Temperatura

--- Reto2/src/retos/Funciones2.java
+++ Reto2/src/retos/Funciones2.java
@@ -1,5 +1,26 @@
 package retos;
 
+import java.util.Scanner;
+
 public class Funciones2 {
 
+	public static void Temperatura() {
+		Scanner sc = new Scanner(System.in);
+		int opcion = -1;
+		Double celsius, fahrenheit;
+		do {
+			opcion = Funciones.dimeEntero(
+					"Selecciona la conversión de temperatura \n1.Celsius a Farhenheit \n2.Farhenheit a Celsius \n0.Salir",
+					sc);
+			if (opcion == 1) {
+				celsius = Funciones.dimeDouble("Escribe los grados celsius", sc);
+				fahrenheit = celsius * 9 / 5 + 32;
+				System.out.println(celsius + "º celsius son " + fahrenheit + " ºF");
+			} else if (opcion == 2) {
+				fahrenheit = Funciones.dimeDouble("Escribe los grados farhenheit", sc);
+				celsius = (fahrenheit - 32) * 5 / 9;
+			}
+
+		} while (opcion != 0);
+	}
 }
