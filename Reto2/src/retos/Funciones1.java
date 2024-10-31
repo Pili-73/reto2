9037bed3437c984789f0b79d4dd4c28f1326ccc2
mariopellicer <marioopelli23@gmail.com>
2024-10-31 09:10:47 +0100

    funciones 1 y mitad de main

--- Reto2/src/retos/Funciones1.java
+++ Reto2/src/retos/Funciones1.java
@@ -1,5 +1,36 @@
 package retos;
 
+import java.util.Scanner;
+
 public class Funciones1 {
 
+	public static double millas_Kilometros(double kilometros) {
+		return kilometros *= 0.62137;
+	}
+
+	public static double kilometros_Millas(double millas) {
+		return millas /= 0.62137;
+	}
+
+	public static void Longitud(double millas, double kilometros) {
+		Scanner sc = new Scanner(System.in);
+		int opcion = -1;
+		do {
+			opcion = Funciones.dimeEntero(
+					"Selecciona la longitud de conversion:\n 1. millas a kilometros\n 2. kilometros a millas\n 0. Salir ",
+					sc);
+			if (opcion == 1) {
+				kilometros = Funciones.dimeDouble("introduce las  millas que quieres convertir", sc);
+
+			} else if (opcion == 2) {
+				millas = Funciones.dimeDouble("introduce los kilometros que quieres convertir", sc);
+			} else if (opcion == 0) {
+				System.out.println("has salido fuera");
+			}
+		} while (opcion != 0);
+	}
+
+	public static void Peso(double kilometros) {
+
+	}
 }
--- Reto2/src/retos/Reto2.java
+++ Reto2/src/retos/Reto2.java
@@ -1,9 +1,36 @@
 package retos;
 
+import java.util.Scanner;
+
 public class Reto2 {
 
 	public static void main(String[] args) {
-		Funciones2.Volumen();
+		Scanner sc = new Scanner(System.in);
+		int conversion = -1;
+		boolean mal=false;
+		do {
+			mal = false;
+			System.out.println("Selecciona el tipo de conversion:");
+			System.out.println("1. Longitud");
+			System.out.println("2. Peso");
+			System.out.println("3. Temperatura");
+			System.out.println("4. Volumen");
+			System.out.println("0. Salir");
+
+			try {
+				String s = sc.nextLine();
+				conversion = Integer.parseInt(s);
+				if (conversion < 0 || conversion > 4) {
+					mal = true;
+					System.out.println("Entre 0 y 4");
+				}
+			} catch (Exception e) {
+				System.out.println("Formato inválido");
+				mal = true;
+			}
+		} while (conversion != 0);
+		
+		sc.close();
 	}
 
 }
