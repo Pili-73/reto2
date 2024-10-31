a096d096ba262b62a406a03c327083e6afb7a347
pilar <piruletaabad2002@gmail.com>
2024-10-31 09:42:12 +0100

    Retocado para que quede igual

--- Reto2/src/retos/Reto2.java
+++ Reto2/src/retos/Reto2.java
@@ -9,7 +9,7 @@ public class Reto2 {
 		int conversion = -1;
 		do {
 			conversion = Funciones.dimeEntero(
-					"Selecciona el tipo de conversion: \n1. Longitud \n2. Peso \n3. Temperatura \n4. Volumen \n0. Salir", sc);
+					"Selecciona el tipo de conversión: \n1. Longitud \n2. Peso \n3. Temperatura \n4. Volumen \n0. Salir", sc);
 			
 			if (conversion == 1) {
 				Funciones1.Longitud();
